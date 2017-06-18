fdd10ab4dc8cbe6b578b10bde9f2bee2bda73c12
Michael Jyms Gutierrez <michael@vielsoft.com>
2017-06-18 15:33:12 +0800

    Update custom css and set settings as first state

--- www/js/state.js
+++ www/js/state.js
@@ -1,4 +1,18 @@
 // Configuration and States
-app.config(function($stateProvider, $urlRouterProvider){
+app.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider){
+
+	// Center Nav Title
+	$ionicConfigProvider.navBar.alignTitle('center');
+
+	// Initialization of States
+	$stateProvider
+	.state('settings',{
+		url:'/settings',
+		templateUrl: 'templates/settings.html',
+		controller: 'mainCtrl'
+	});
+
+	$urlRouterProvider.otherwise('/settings');
+
 	
 });
