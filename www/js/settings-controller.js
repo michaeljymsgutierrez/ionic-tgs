4f24d6f89dc478624b78b61fc06b0208c3840ea8
Chaelgutierrez <michael@vielsoft.com>
2017-06-20 07:56:32 +0800

    Added toast service for store settings

--- www/js/settings-controller.js
+++ www/js/settings-controller.js
@@ -1,5 +1,5 @@
 // Settings Controller
-app.controller('settingsCtrl',function($scope, $ionicPopup, $rootScope, $cordovaSQLite, $location){
+app.controller('settingsCtrl',function($scope, $ionicPopup, $rootScope, $cordovaSQLite, $location, Toast){
 
 	// Initialize store object for settings
 	// rootScope  is used due to hiding of popup
@@ -28,35 +28,41 @@ app.controller('settingsCtrl',function($scope, $ionicPopup, $rootScope, $cordova
 
 	// Insert-Update function for store settings
 	$scope.save_store_settings = function(){
-
-		$cordovaSQLite.execute(db,'SELECT * FROM store_settings').then(function(res){
-			if(res.rows.length == 0){
-				// Insert store settings if table item length is 1
-				$cordovaSQLite.execute(db,'INSERT INTO store_settings (store_code, store_branch, store_address, store_type, store_manager) VALUES (?,?,?,?,?)',
-				[$rootScope.store_form.store_code, $rootScope.store_form.store_branch, $rootScope.store_form.store_address, $rootScope.store_form.store_type, $rootScope.store_form.store_manager])
-				.then(function(res){
-					console.log(res);
-				},function(err){
-					// Error inserting on table
-					console.log(err);
-				});
-			}
-			else{
-				// Update store settings when table item length is 0
-				id = res.rows.item(0).id;
-				$cordovaSQLite.execute(db,"UPDATE store_settings SET store_code = ?, store_branch = ?, store_address = ?, store_type = ?, store_manager = ? WHERE id = ?",
-				[$rootScope.store_form.store_code, $rootScope.store_form.store_branch, $rootScope.store_form.store_address, $rootScope.store_form.store_type, $rootScope.store_form.store_manager,id])
-				.then(function(res){
-					console.log(res);
-				},function(err){
-					// Error updating table
-					console.log(err);
-				});
-			}
-		},function(err){ 
-			// Error checking store settings table
-			console.log(err); 
-		});
-	};
+		if($rootScope.store_form.store_code == undefined || $rootScope.store_form.store_branch == undefined || $rootScope.store_form.store_address == undefined || $rootScope.store_form.store_type == undefined || $rootScope.store_form.store_manager == undefined){
+			Toast.show("Please fill up all fields before saving . . .","long","center");
+		}
+		else{
+			$cordovaSQLite.execute(db,'SELECT * FROM store_settings').then(function(res){
+				if(res.rows.length == 0){
+					// Insert store settings if table item length is 1
+					$cordovaSQLite.execute(db,'INSERT INTO store_settings (store_code, store_branch, store_address, store_type, store_manager) VALUES (?,?,?,?,?)',
+					[$rootScope.store_form.store_code, $rootScope.store_form.store_branch, $rootScope.store_form.store_address, $rootScope.store_form.store_type, $rootScope.store_form.store_manager])
+					.then(function(res){
+						Toast.show("Store settings successfully saved . . .","long","center");
+						console.log(res);
+					},function(err){
+						// Error inserting on table
+						console.log(err);
+					});
+				}
+				else{
+					// Update store settings when table item length is 0
+					id = res.rows.item(0).id;
+					$cordovaSQLite.execute(db,"UPDATE store_settings SET store_code = ?, store_branch = ?, store_address = ?, store_type = ?, store_manager = ? WHERE id = ?",
+					[$rootScope.store_form.store_code, $rootScope.store_form.store_branch, $rootScope.store_form.store_address, $rootScope.store_form.store_type, $rootScope.store_form.store_manager,id])
+					.then(function(res){
+						Toast.show("Store settings successfully updated . . .","long","center");
+						console.log(res);
+					},function(err){
+						// Error updating table
+						console.log(err);
+					});
+				}
+			},function(err){ 
+				// Error checking store settings table
+				console.log(err); 
+			});
+		};
+	}
 
 });
