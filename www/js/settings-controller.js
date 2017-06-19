cb367b9fbe00f256ee9a19c888191a8c9bd4285e
Chaelgutierrez <michael@vielsoft.com>
2017-06-19 12:01:03 +0800

    Added object function for popup button settings controller

--- www/js/settings-controller.js
+++ www/js/settings-controller.js
@@ -1,15 +1,23 @@
 // Settings Controller
-app.controller('settingsCtrl',function($scope, $ionicPopup){
+app.controller('settingsCtrl',function($scope, $ionicPopup, $rootScope){
 
+	// Popup show store type
 	$scope.storeType =  function(){
-		$ionicPopup.show({
+		// Use rootScope for closing Custom Popup
+		$rootScope.myPopup = $ionicPopup.show({
 			title: '<p align="left" style="font-size: 100px !important"><font color="black">Store Type</font></p>',
 			templateUrl: 'templates/template-settings/store-type.html'
 		});
 	};
 
-	$scope.closeStoreType = function(){
-		console.log(1);
+	// Popup Hide Object function
+	$scope.closeStoreType = {
+		cancel: function(){
+			$rootScope.myPopup.close();
+		},
+		okay: function(){
+			$rootScope.myPopup.close();
+		}
 	};
 
 
