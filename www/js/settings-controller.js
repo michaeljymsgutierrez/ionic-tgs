943a2582aa0ed54e744e861b71459689fdf05e55
Chaelgutierrez <michael@vielsoft.com>
2017-06-20 12:53:51 +0800

    Added save-update language type on select - survey language settings

--- www/js/settings-controller.js
+++ www/js/settings-controller.js
@@ -5,6 +5,12 @@ app.controller('settingsCtrl',function($scope, $ionicPopup, $rootScope, $cordova
 	// rootScope  is used due to hiding of popup
 	$rootScope.store_form = { };
 
+	// Survey Language value on load
+	$scope.survey_language = {
+		value1: true,
+		value2: false
+	}
+
 	// Platform ready code execution 
 	$ionicPlatform.ready(function(){
 			//  Load all current settings from db when state on store settings
@@ -45,7 +51,7 @@ app.controller('settingsCtrl',function($scope, $ionicPopup, $rootScope, $cordova
 	};
 
 	// Insert-Update function for store settings
-	$scope.save_store_settings = function(){ console.log($rootScope.store_form);
+	$scope.save_store_settings = function(){
 		if($rootScope.store_form.store_code == undefined || $rootScope.store_form.store_branch == undefined || $rootScope.store_form.store_address == undefined || $rootScope.store_form.store_type == undefined || $rootScope.store_form.store_manager == undefined){
 			// 
 			Toast.show("Please fill up all fields before saving . . .","long","center");
@@ -92,5 +98,47 @@ app.controller('settingsCtrl',function($scope, $ionicPopup, $rootScope, $cordova
 			});
 		};
 	}
+
+	// Survey Language Function
+	$scope.select_language = function(data){
+		// Condition fo switching checkbox
+		if(data == "English"){
+			$scope.survey_language.value1 = true;
+			$scope.survey_language.value2 = false;
+		}
+		else if(data == "Tagalog"){
+			$scope.survey_language.value1 = false;
+			$scope.survey_language.value2 = true;
+		}
+		// Save and Update language type
+		$cordovaSQLite.execute(db,"SELECT * FROM survey_language").then(function(res){
+			if(res.rows.length == 0){
+				$cordovaSQLite.execute(db,"INSERT INTO survey_language (language) VALUES (?)",[data])
+				.then(function(res){
+					Toast.show("Successfully saved language type . . .","long","center");
+					console.log(res);
+				},function(err){
+					// Error insert data
+					console.log(err);
+				});
+			}
+			else{
+				id = res.rows.item(0).id;
+				$cordovaSQLite.execute(db,"UPDATE survey_language SET language = ? WHERE id = ?",[data, id])
+				.then(function(res){
+					Toast.show("Successfully updated language type . . .","long","center");
+					console.log(res);
+				},function(err){
+					// Error update data
+					console.log(err);
+				});
+			}
+
+		},function(err){
+			// Error fetching data survey language
+			console.log(err);
+		});
+
+	};
 
 });
