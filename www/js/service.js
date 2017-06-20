d91b597e5a7c59bf94f070510f1181d4fdb2a7ab
Chaelgutierrez <michael@vielsoft.com>
2017-06-20 19:14:48 +0800

    Added dateformmatter service

--- www/js/service.js
+++ www/js/service.js
@@ -6,3 +6,16 @@ app.service('Toast',function($cordovaToast){
 		$cordovaToast.show(msg,duration,pos);
 	};
 });
+
+// Service for formatting date 
+app.service('dateFormatter',function($filter){
+	// Convert ISO to date only
+	this.toDate = function(dt){
+		return $filter('date')(dt,'yyyy-MM-dd');
+	}
+
+	// Convert ISO to 24 HR format
+	this.toTime = function(dt){
+	   return $filter('date')(dt,'H:mm');
+	}
+});
