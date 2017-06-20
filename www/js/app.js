9071ad0e5ce16f5db7283ef72cd983bd86a900f5
Chaelgutierrez <michael@vielsoft.com>
2017-06-20 19:47:18 +0800

    Added payday weekday tbl initialization

--- www/js/app.js
+++ www/js/app.js
@@ -38,5 +38,13 @@ app.run(function($ionicPlatform, $cordovaSQLite, $ionicPickerI18n) {
           console.log(err);
        });
 
+       // Initialize table for payday weekday schedule
+       $cordovaSQLite.execute(db,'CREATE TABLE IF NOT EXISTS payday_weekday(id ineteger primary key, payday_weekday_date text, payday_weekday_start text, payday_weekday_end text)')
+       .then(function(res){
+          console.log(res);
+       },function(err){
+          console.log(err);
+       });
+
     });
 });
