10f5e110244ade4afe027c28195946bb3c59da79
Chaelgutierrez <michael@vielsoft.com>
2017-06-22 08:25:21 +0800

    Update sidemenu styling

--- platforms/android/assets/www/js/survey-controller.js
+++ platforms/android/assets/www/js/survey-controller.js
@@ -1,18 +1,28 @@
 // Survey Controller
-app.controller('surveyCtrl',function($ionicSideMenuDelegate, $scope, $ionicHistory, $rootScope){
+app.controller('surveyCtrl',function($ionicSideMenuDelegate, $scope, $ionicHistory, $rootScope, $window){
 
 	// Toggle Sidemenu on survey controller
 	$rootScope.toggleLeft = function(){
         $ionicSideMenuDelegate.toggleLeft();
     };
 
+	// Renable side menu  
+	$ionicSideMenuDelegate.canDragContent(true);
+
     // Clear History after start app 
     $ionicHistory.clearHistory();
 	
-   $rootScope.$watch(function(){
-      return $ionicSideMenuDelegate.getOpenRatio();
-   },function (ratio){
-	  $rootScope.sidemenuopened = (ratio == 1);
-	});
+	// Watch side menu event throug rootscope
+   	$rootScope.$watch(function(){
+   		// Return ratio
+      	return $ionicSideMenuDelegate.getOpenRatio();
+    },function (ratio){
+    	// Broadcast the value
+	  	$rootScope.sidemenuopened = (ratio == 1);
+    });
+
+   	// Iniailize ease access data from localstorage
+    $rootScope.store_name = $window.localStorage.getItem('store_name');
+    $rootScope.store_type = $window.localStorage.getItem('store_type');
 
 });
