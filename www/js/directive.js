fad0b74e206d36a7fd5932e7e747000f72ec55b1
Michael Jyms Gutierrez <michael@vielsoft.com>
2017-06-28 07:29:26 +0800

    Added linebreaker 2 directive

--- www/js/directive.js
+++ www/js/directive.js
@@ -1,8 +1,17 @@
 // Include all directives
 
+// Four breaks Directive
 app.directive('lineBreaker',function(){
 	return {
 		restrict: 'E',
 		template: "<br/><br/><br/<br/>"
+	}
+});
+
+// Two breaks Directive
+app.directive('lineBreaker2',function(){
+	return {
+		restrict: 'E',
+		template: "<br/><br/>"
 	}
 });
