20d72b7f8b0f46e0ef30ec6091a2e1371a7d7274
Chaelgutierrez <michael@vielsoft.com>
2017-07-10 10:36:44 +0800

    Added emoji stripper directive

--- www/js/directive.js
+++ www/js/directive.js
@@ -23,3 +23,24 @@ app.directive('lineBreaker3',function(){
 		template: "<br/><br/><br/>"
 	}
 });
+
+// Stripped emoji
+app.directive('noEmoji',function(){
+    //Custom Directive for checking negative value
+    return {
+        restrict: 'A',
+        require: '?ngModel',
+        link: function(scope, element , attrs, ngModel){
+            scope.$watch(attrs.ngModel, function(val){
+                if(val != ""){
+                  // Remove occurence of emoji
+                  try{
+                  	  var text = val.replace(/([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2694-\u2697]|\uD83E[\uDD10-\uDD5D])/g, '')
+	                  ngModel.$setViewValue(text);
+	                  ngModel.$render();
+                  }catch(err) {}
+                }
+            });
+        }
+    }
+});
